49c65a94ce1c0111ab6d141bbc71f74486c863a0
MilesSobhani <38140009+MilesSobhani@users.noreply.github.com>
2022-03-31 10:29:49 -0500

    added updating  state for correct letters updated comments

--- src/App.js
+++ src/App.js
@@ -15,13 +15,14 @@ const newLayout = {
 }
 
 class App extends Component {
+  
 constructor(props){
   super(props);
   this.state = {
     answer:'wordl',
     currentRow: 0,
     currentLetter: 0,
-    currentGuess: ['','','','',''],
+    currentGuess: [[],[]],
     totalGuesses: 6,
     wordLength: 5,
     gameBoard: [],
@@ -31,7 +32,7 @@ constructor(props){
   this.onKeyPress = this.onKeyPress.bind(this) 
 }
 
-//Handles all letter inputs
+//Handles all keyboard inputs
 onKeyPress(button){
   if(button !== 'Submit' && button !== 'Delete'){
     
@@ -39,16 +40,27 @@ onKeyPress(button){
     let guessUpdate = this.state.currentGuess;
     let nextBoard = this.state.gameBoard;
 
-      nextBoard[this.state.currentRow] = this.state.currentGuess;
+      nextBoard[this.state.currentRow] = this.state.currentGuess[0];
+
+      //Restricts board changes to within the length of the word
       if(this.state.currentLetter < this.state.wordLength){
+        guessUpdate[0][this.state.currentLetter] = button;
         
-        guessUpdate[this.state.currentLetter] = button;
-        
+        //Changes state if the guessed letter is in the answer
+        if(this.state.answer.includes(button)){
+          guessUpdate[1][this.state.currentLetter] = 1
+
+          //Changes state if the guessed letter is in the correct spot
+          if(this.state.answer[this.state.currentLetter] === button){
+            guessUpdate[1][this.state.currentLetter] = 2
+          }
+        }
+
         this.setState({
           gameBoard: nextBoard,
           currentGuess: guessUpdate,
           currentLetter: this.state.currentLetter + 1
-        });
+        }, () => console.log(this.state.currentGuess));
       }
 //Handles Delete and Enter inputs
     } else if(button === 'Submit'){
@@ -60,12 +72,18 @@ onKeyPress(button){
 
 
 componentDidMount(){
-  //Builds the initial gameBoard
-  let newBoard = []
+  //Builds the initial gameBoard and currentGuess state
+  let newBoard = [];
+  let firstGuess = [];
+  firstGuess.push([],[]);
   for(var i = 0; i < this.state.totalGuesses; i++){
     newBoard.push([]);
     for(var j = 0; j < this.state.wordLength; j++){
       newBoard[i].push('');
+      if(i === 0){
+        firstGuess[0].push('');
+        firstGuess[1].push(0);
+      }
     }
   }
   //Generates initial answer and applies it to state
@@ -73,10 +91,21 @@ componentDidMount(){
   this.setState({
     gameBoard: newBoard,
     answer: newAnswer,
-    doneLoading: true
+    doneLoading: true,
+    currentGuess: firstGuess
+  });
+
+}
+
+buildGuess(){
+  let newGuess = [[],[]]
+  for(var x = 0; x < this.state.wordLength; x ++){
+      newGuess[0].push('');
+      newGuess[1].push(0);
+  }
+  this.setState({
+    currentGuess:newGuess
   })
-
-
 }
 
 //Handles the delete button
@@ -99,16 +128,17 @@ handleDelete(){
 
 //Handles the enter function, puts the guess on the state game board
 handleGuess(){
-  let guessString = this.state.currentGuess.join('');
+  
+
+
+  let guessString = this.state.currentGuess[0].join('');
   console.log(guessString)
   if(data.data2.includes(guessString) || data.data1.includes(guessString)){
     console.log("that's actually a word")
     this.setState({
       currentRow:this.state.currentRow + 1,
-      currentLetter: 0,
-      currentGuess: ['','','','','']
-
-    })
+      currentLetter: 0
+    },this.buildGuess())
 
   } else {
     console.log('Not a word Sorry');
